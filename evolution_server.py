2b5b0fea83c1139b5b90048df6ac78fd65c94ffa
villinvic <vvillin@hotmail.fr>
2021-03-18 22:19:33 +0900

    3-18

--- evolution_server.py
+++ evolution_server.py
@@ -25,8 +25,8 @@ import socket
 
 class EvolutionServer:
 
-    def __init__(self, ID, env_id='Pong-ram-v0', collector_ip=None, traj_length=128, batch_size=1, max_train=7,
-                 early_stop=7, round_length=200, min_eval=5000, min_games=7, subprocess=True, mutation_rate=0.5):
+    def __init__(self, ID, env_id='Pong-ram-v0', collector_ip=None, traj_length=128, batch_size=1, max_train=10,
+                 early_stop=7, round_length=200, min_eval=5000, min_games=10, subprocess=True, mutation_rate=0.5):
         if collector_ip is None:
             self.ip = socket.gethostbyname(socket.gethostname())
         else:
