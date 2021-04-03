4982486fb9677292495d99b5d684472ca309a352
villinvic <vvillin@hotmail.fr>
2021-04-04 03:09:48 +0900

    4-02

--- evolution_server.py
+++ evolution_server.py
@@ -25,7 +25,7 @@ import socket
 
 class EvolutionServer:
 
-    def __init__(self, ID, env_id='Pong-ram-v0', collector_ip=None, traj_length=10, batch_size=16, max_train=4000,
+    def __init__(self, ID, env_id='Pong-ram-v0', collector_ip=None, traj_length=10, batch_size=16, max_train=3000,
                  early_stop=100, round_length=300, min_eval=1, min_games=1, subprocess=True, mutation_rate=0.1):
         if collector_ip is None:
             self.ip = socket.gethostbyname(socket.gethostname())
@@ -167,7 +167,7 @@ class EvolutionServer:
                         gaussian_noise = np.random.normal(loc=0, scale=intensity, size=q['pi'][i].shape)
                         q['pi'][i] += gaussian_noise
 
-                gaussian_noise = np.abs(np.random.normal(loc=0, scale=2, size=q['r'].shape))
+                gaussian_noise = np.abs(np.random.normal(loc=0, scale=1.2, size=q['r'].shape))
                 q['r'] = np.clip(q['r'] * gaussian_noise, 1e-4, np.inf)
 
     def eval(self, player: Individual, min_frame):
--- population.py
+++ population.py
@@ -41,8 +41,7 @@ class Individual:
 class LightIndividual:
     def __init__(self, goal_dim, generation=1):
 
-        self.reward_weight = 0.05 * log_uniform(0, 10, (goal_dim,), base=2) / 2.0**10
-        self.reward_weight[0] = 1
+        self.reward_weight = log_uniform(0, 12, (goal_dim,), base=2) / 2.0**11
         self.behavior_stats = {}
         self.gen = generation
         self.model_weights = None
