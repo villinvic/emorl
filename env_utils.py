693b65974058a677e42d7715b1a04a41ec09a343
villinvic <vvillin@hotmail.fr>
2021-06-17 10:07:14 +0200

    new objectives2

--- env_utils.py
+++ env_utils.py
@@ -415,6 +415,8 @@ class Tennis(EnvUtil):
     def aim_quality(self, full_obs):
         ball_x = full_obs[-self.state_dim+3]
         ball_y = full_obs[-self.state_dim+4]
+        print(ball_x, ball_y)
+        print(full_obs[-2*self.state_dim+3], full_obs[-2*self.state_dim+4])
         vector = complex(ball_y - full_obs[-2*self.state_dim+4], ball_x - full_obs[-2*self.state_dim+3])
         angle = np.angle(vector)
 
@@ -426,7 +428,7 @@ class Tennis(EnvUtil):
 
         quality = np.abs(ball_x + deviation - opp_x)
 
-        print(ball_y, vector, angle, opp_y, deviation, quality)
+        # print(ball_y, vector, angle, opp_y, deviation, quality)
 
         return quality
 
