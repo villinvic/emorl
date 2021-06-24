c3f88a5187e5901a5bfcf9412d487b50049eb80c
villinvic <vvillin@hotmail.fr>
2021-06-24 12:27:51 +0200

    fix hollandgpu

--- evolution_server.py
+++ evolution_server.py
@@ -27,13 +27,7 @@ import os
 class EvolutionServer:
 
     def __init__(self, ID, env_id='Pong-ram-v0', collector_ip=None, traj_length=10, batch_size=16, max_train=12,
-                 early_stop=100, round_length=300, min_eval=100, min_games=2, subprocess=True, mutation_rate=0.5,
-                 gpu=False):
-
-        if gpu:
-            os.environ["CUDA_VISIBLE_DEVICES"] = str(ID) if ID < 4 else '-1'
-        else:
-            os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+                 early_stop=100, round_length=300, min_eval=100, min_games=2, subprocess=True, mutation_rate=0.5):
 
         if collector_ip is None:
             self.ip = socket.gethostbyname(socket.gethostname())
